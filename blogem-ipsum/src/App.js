2f26e6f7382633f280501a4c920cba640e5a32f8
Ivan Mitkov <ivan.mitkov@gmail.com>
2022-12-14 06:29:49 +0200

    add boiler plate for the tutorial

--- blogem-ipsum/src/App.js
+++ blogem-ipsum/src/App.js
@@ -5,9 +5,9 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import logo from "./logo.svg";
+
+import { Posts } from "./Posts";
 import "./App.css";
 
 const queryClient = new QueryClient();
@@ -16,20 +16,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Blog Posts</h1>
+        <Posts />
       </div>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
