b34b4b19be18939dd61e7ff18d72813c8589302e
Ivan Mitkov <ivan.mitkov@gmail.com>
2022-12-14 08:09:40 +0200

    add pagination

--- blogem-ipsum/src/Posts.jsx
+++ blogem-ipsum/src/Posts.jsx
@@ -5,20 +5,20 @@ import { useQuery } from "@tanstack/react-query";
 
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(page) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1); // for this api pages starts at 1
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
   const { isLoading, isError, data, error } = useQuery({
-    queryKey: ["posts"],
+    queryKey: ["posts", currentPage],
     queryFn: fetchPosts,
     staleTime: 20000,
   });
@@ -45,11 +45,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage < 1}
+          onClick={() => {
+            setCurrentPage((currentPage) => currentPage - 1);
+          }}
+        >
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage >= maxPostPage - 1}
+          onClick={() => {
+            setCurrentPage((currentPage) => currentPage + 1);
+          }}
+        >
           Next page
         </button>
       </div>
